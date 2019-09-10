b898f8fea86bd43008ee3bc25eedd63beb56f54e
Anthony Yonta <atischillin@gmail.com>
2019-09-09 22:00:29 -0400

    created assets / JS, CSS, Index files and base template

---
+++ assets/javascript/question.js
@@ -0,0 +1,23 @@
+var quizQuestions = [
+    {
+        question: "Famous Warrior in WOW, Known for charging in recklessly",
+        choices: ["leeroy Jenkins","Mulana","Snow Fox","Beeroism"],
+        correctAnswer:  "leeroy Jenkins"
+
+    },
+
+    {
+        question: "Which of the following is NOT an orc clan",
+        choices: ["Bloodfang","Dragonmaw","Warsong","FrostWolf"],
+        correctAnswer:  "Bloodfang"
+
+    },
+
+    {
+        question: "Which night elf was appointed the jailor of Illidan Stormrage?",
+        choices: ["Maiev Shadowsong","Califax","Jarod Shadowsong","Malfurion Stormrage"],
+        correctAnswer:  "Maiev Shadowsong"
+
+    },
+
+];
